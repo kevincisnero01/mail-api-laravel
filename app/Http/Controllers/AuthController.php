40a5b804178c1d461965b75541d580125bfce91c
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-12-21 19:44:33 -0400

    Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -23,5 +24,27 @@ class AuthController extends Controller
             'message' => 'Registro de usuario con exito',
             'data' => $user,
         ]);
+    }
+
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:6|max:30'
+        ]);
+
+        if(Auth::attempt($credentials))
+        {
+            return response()->json([
+                'message' => 'Inicio de sesión con exito',
+                'token' => $request->user()->createToken($request->email)->plainTextToken
+            ]);
+
+        };
+
+        return response()->json([
+            'message' => 'Las credenciales proporcionadas no coinciden con nuestros registros',
+        ],401);
     }
 }
